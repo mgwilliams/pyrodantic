e0edb59450794c69a7cde5c086ec3e739b80939f
Matthew Williams <matthew@yellowpacket.com>
2020-06-19 09:35:33 -0400

    version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyrodantic",
-    version="0.0.3",
+    version="0.0.4",
     author="Matthew Williams",
     author_email="mgwilliams@gmail.com",
     description="Pydantic models for Firestore",
